66ef87df3c2de2cd619ded7315f6834ad7305b5b
Kawaiiow <poohsn1357@gmail.com>
2024-11-06 05:07:05 +0700

    feat (command) : Add log command for inspect data records

--- src/requirement/main/it_ticket/it_ticket/main.py
+++ src/requirement/main/it_ticket/it_ticket/main.py
@@ -1,9 +1,10 @@
 import os
 import discord
+import enum
 
 from dotenv import load_dotenv
-from discord import ChannelType
 from discord.ext import commands
+from tdb import ticket_tab
 
 load_dotenv()
 intents : discord.Intents = discord.Intents.default()
@@ -31,9 +32,16 @@ async def on_ready():
 
 #   respond message
 @client.event
-async def on_message(message):
+async def on_message(message : discord.Message):
     if message.author == client.user:
         return
+
+    cur_channel = message.channel
+    if cur_channel.type == discord.ChannelType.private_thread:
+        tab = ticket_tab(message.guild.id)
+        tab.ft_update_participant(cur_channel.id, message.author.name)
+
+
 
 # latency
 @client.tree.command(name="ping", description="Check the bot's latency")
@@ -54,6 +62,55 @@ async def clean_thread(interaction : discord.Interaction):
 @client.tree.command(name="cat", description="cat everywhere")
 async def cat(interaction : discord.Interaction):
     await interaction.response.send_message(f"{interaction.channel.threads}")
+
+class console_opt(enum.Enum):
+    log = 1
+    clear = 2
+
+@client.tree.command(name="console", description="list all log of thread")
+async def lst_thrd(interaction : discord.Interaction, option : console_opt):
+    if not interaction.user.guild_permissions.administrator:
+        await interaction.response.send_message("## Console avaliable for administrator only!")
+        return
+    tab = ticket_tab(interaction.guild_id)
+    match option:
+        case console_opt.log:
+            if not tab:
+                await interaction.response.send_message("No Table Exist")
+            else:
+                msg = ""
+                for rec in tab.ft_get_all():
+                    msg += str(rec) + "\n"
+                await interaction.response.send_message(msg if msg else "Nothing exist in table")
+        case console_opt.clear:
+            tab.ft_clear()
+            await interaction.response.send_message("Table have been clear")
+        case _:
+            await interaction.response.send_message("Option Error")
+
+@client.tree.command(name="log_all", description="log every records in the server")
+async def cb(interaction : discord.Interaction):
+    tab = ticket_tab(interaction.guild_id)
+    if not tab:
+        await interaction.response.send_message("No Table Exist")
+    else:
+        msg = ""
+        for rec in tab.ft_get_all():
+            msg += str(rec) + "\n"
+        await interaction.response.send_message(msg if msg else "Nothing exist in table")
+
+@client.tree.command(name="log_user", description="log specific user")
+async def cb(interaction : discord.Interaction, user : discord.User):
+    tab = ticket_tab(interaction.guild_id)
+    res = tab.ft_get_by_user(user.id)
+    await interaction.response.send_message(res)
+
+@client.tree.command(name="log_thread", description="Log specific thread")
+async def cb(interaction : discord.Interaction, thread : discord.Thread):
+    tab = ticket_tab(interaction.guild_id)
+    res = tab.ft_get_by_thread(thread.id)
+    await interaction.response.send_message(res)
+
 
 try:
     # import initialize function of embed
